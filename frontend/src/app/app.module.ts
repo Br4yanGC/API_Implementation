c86224ddb8f6308edf2c9bb16fd55868de687d9c
Br4yanGC <brayan20160113@gmail.com>
2021-10-11 10:30:35 -0500

    Change 10

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MembersTableComponent } from './members-table/members-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MembersTableComponent
   ],
   imports: [
     BrowserModule
