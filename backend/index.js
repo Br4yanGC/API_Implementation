2740841171f77487368c2612d4a23c7d08d1f2d0
Br4yanGC <brayan20160113@gmail.com>
2021-10-11 10:14:01 -0500

    Change 8

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+
+var mysql = require('mysql');
+var cors = require('cors');
+
+app.use(cors());
+
+app.get('/sayhi', hello_world);
+
+function hello_world(req, res){
+    res.send("Hello world, this is my API!!");
+}
+
+app.get('/menu', getMenu);
+
+function getMenu(req, res){
+
+    // Step 0: Definir la conexion a la BD
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'utec',
+        password: '1234567890',
+        //database: 'my_chess_club'
+        database: 'restaurant'
+    });
+
+    // Step 1: Establecer la conexion
+    connection.connect();
+
+    // Step 2: Mandar el query
+    //var myQuery =   " SELECT member_id, fullname, birthday, ranking, " +
+    //                " gender, email, created_date, modified_date " +
+    //                " FROM member; "
+
+    var myQuery =   " SELECT dish, price, created_date, modified_date " +
+                    " FROM menu; "
+    connection.query(myQuery, function(error, results, fields){
+        // Ya tengo el resultado del query en `results`
+        
+        // Step 3: Procesar el resultado de la BD
+        res.send(results);
+
+        // Step 4: Cerrar la conexion
+        connection.end();
+    });   
+}
+
+app.listen(3000, function(){
+    console.log("Server started in port 3000!!!")
+})
